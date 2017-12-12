8ebfb49447f869545bdf7d43596f5750881f3dfa
Jakub Linhart <jakub@postsharp.net>
2017-12-12 22:14:44 +0100

    Adding concurrency tests for targeting.

--- Infusion.LegacyApi.Tests/TargetingTests.cs
+++ Infusion.LegacyApi.Tests/TargetingTests.cs
@@ -15,42 +15,52 @@ namespace Infusion.LegacyApi.Tests
         [TestMethod]
         public void Can_wait_for_target_When_no_last_action()
         {
-            var testProxy = new InfusionTestProxy();
-            var task = Task.Run(() => testProxy.Api.WaitForTarget(TimeSpan.MaxValue));
-            testProxy.Api.WaitForTargetStartedEvent.WaitOne(100).Should().BeTrue();
-            testProxy.ServerPacketHandler.HandlePacket(TargetCursorPackets.TargetCursor);
+            ConcurrencyTester.Run(() =>
+            {
+                var testProxy = new InfusionTestProxy();
+                var task = Task.Run(() => testProxy.Api.WaitForTarget(TimeSpan.MaxValue));
+                testProxy.Api.WaitForTargetStartedEvent.WaitOne(100).Should().BeTrue();
+                testProxy.ServerPacketHandler.HandlePacket(TargetCursorPackets.TargetCursor);
 
-            task.Wait(100).Should().BeTrue();
+                task.Wait(100).Should().BeTrue();
+            });
         }
 
         [TestMethod]
         public void Can_wait_for_target_after_last_action_When_TargetCursor_arrives_before_waiting_starts()
         {
-            var testProxy = new InfusionTestProxy();
+            ConcurrencyTester.Run(() =>
+            {
+                var testProxy = new InfusionTestProxy();
 
-            testProxy.Api.NotifyAction(DateTime.UtcNow.AddMilliseconds(-1));
-            testProxy.ServerPacketHandler.HandlePacket(TargetCursorPackets.TargetCursor);
+                testProxy.Api.NotifyAction(DateTime.UtcNow.AddMilliseconds(-1));
+                testProxy.ServerPacketHandler.HandlePacket(TargetCursorPackets.TargetCursor);
 
-            bool waitResult = false;
-            var task = Task.Run(() => waitResult = testProxy.Api.WaitForTarget(TimeSpan.MaxValue));
+                bool waitResult = false;
+                var task = Task.Run(() => waitResult = testProxy.Api.WaitForTarget(TimeSpan.MaxValue));
 
-            task.Wait(100).Should().BeTrue();
-            waitResult.Should().BeTrue();
+                task.Wait(100).Should().BeTrue();
+                waitResult.Should().BeTrue();
+            });
         }
 
         [TestMethod]
         public void Can_wait_for_target_after_last_action_When_TargetCursor_arrives_before_waiting_starts_and_failure_messages_are_specified()
         {
-            var testProxy = new InfusionTestProxy();
+            ConcurrencyTester.Run(() =>
+            {
+                var testProxy = new InfusionTestProxy();
 
-            testProxy.Api.NotifyAction(DateTime.UtcNow.AddMilliseconds(-1));
-            testProxy.ServerPacketHandler.HandlePacket(TargetCursorPackets.TargetCursor);
+                testProxy.Api.NotifyAction(DateTime.UtcNow.AddMilliseconds(-1));
+                testProxy.ServerPacketHandler.HandlePacket(TargetCursorPackets.TargetCursor);
 
-            bool waitResult = false;
-            var task = Task.Run(() => waitResult = testProxy.Api.WaitForTarget(TimeSpan.MaxValue, "some", "failure", "messages"));
+                bool waitResult = false;
+                var task = Task.Run(() =>
+                    waitResult = testProxy.Api.WaitForTarget(TimeSpan.MaxValue, "some", "failure", "messages"));
 
-            task.Wait(100).Should().BeTrue();
-            waitResult.Should().BeTrue();
+                task.Wait(100).Should().BeTrue();
+                waitResult.Should().BeTrue();
+            });
         }
 
         [TestMethod]
@@ -69,17 +79,21 @@ namespace Infusion.LegacyApi.Tests
         [TestMethod]
         public void Can_terminate_before_target_because_fail_message_received()
         {
-            var testProxy = new InfusionTestProxy();
+            ConcurrencyTester.Run(() =>
+            {
+                var testProxy = new InfusionTestProxy();
 
-            testProxy.Api.NotifyAction(DateTime.UtcNow.AddMilliseconds(-1));
+                testProxy.Api.NotifyAction(DateTime.UtcNow.AddMilliseconds(-1));
 
-            testProxy.ServerPacketHandler.HandlePacket(SpeechPackets.FailureMessageFromServer);
+                testProxy.ServerPacketHandler.HandlePacket(SpeechPackets.FailureMessageFromServer);
 
-            bool waitResult = true;
-            var task = Task.Run(() => waitResult = testProxy.Api.WaitForTarget(TimeSpan.MaxValue, "failure message"));
-            task.Wait(100).Should().BeTrue();
+                bool waitResult = true;
+                var task = Task.Run(
+                    () => waitResult = testProxy.Api.WaitForTarget(TimeSpan.MaxValue, "failure message"));
+                task.Wait(100).Should().BeTrue();
 
-            waitResult.Should().BeFalse();
+                waitResult.Should().BeFalse();
+            });
         }
     }
 }

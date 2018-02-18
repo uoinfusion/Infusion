78c8f46ba0d80ffb786adaaee37249af82a6900a
Jakub Linhart <jakub.linhart@gmail.com>
2018-02-18 18:14:16 +0100

    New internal release version 1.0.30-alpha.

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.0.29.0")]
-[assembly: AssemblyFileVersion("1.0.29.0")]
+[assembly: AssemblyVersion("1.0.30.0")]
+[assembly: AssemblyFileVersion("1.0.30.0")]

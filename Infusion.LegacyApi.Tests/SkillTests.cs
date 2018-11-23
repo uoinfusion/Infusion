2cae4994d6cdb3eaa356dbaf74cdad5df4b0fc4d
Jakub Linhart <jakub.linhart@gmail.com>
2018-11-23 12:55:58 +0100

    Fixing test related to Alchemy skill gain problem.

--- Infusion.LegacyApi.Tests/SkillTests.cs
+++ Infusion.LegacyApi.Tests/SkillTests.cs
@@ -28,22 +28,16 @@ namespace Infusion.LegacyApi.Tests
         }
 
         [TestMethod]
-        public void Single_skill_0_change_raises_event()
+        public void Single_skill_0_change_raises_event_as_Alchemy()
         {
             var testProxy = new InfusionTestProxy();
             var journal = testProxy.Api.CreateEventJournal();
 
-            // initialize skill value, SkillChangedEvent requires it
-            testProxy.PacketReceivedFromServer(new byte[] { 0x3A, 0x00, 0x0B, 0xFF, 0x00, 0x02, 0x02, 0xC9, 0x02, 0xC9, 0x00, });
-
-            testProxy.Api.UseSkill(Skill.AnimalLore);
-
-            // Sphere 0.99gz seems to send skill 0 sometimes, instead of actual skill or the skill mapping is not completely correct
-            // in Infusion, not sure...
+            testProxy.PacketReceivedFromServer(new byte[] { 0x3A, 0x00, 0x0B, 0xFF, 0x00, 0x00, 0x02, 0xC9, 0x02, 0xC9, 0x00, });
             testProxy.PacketReceivedFromServer(new byte[] { 0x3A, 0x00, 0x0B, 0xFF, 0x00, 0x00, 0x02, 0xCA, 0x02, 0xCA, 0x00, });
 
             journal.OfType<SkillChangedEvent>().Should().NotBeEmpty();
-            journal.OfType<SkillChangedEvent>().First().Skill.Should().Be(Skill.AnimalLore);
+            journal.OfType<SkillChangedEvent>().First().Skill.Should().Be(Skill.Alchemy);
         }
     }
 }

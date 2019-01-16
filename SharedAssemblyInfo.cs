575070fe013690708fbede69125e474d54478089
Jakub Linhart <jakub.linhart@gmail.com>
2019-01-16 14:32:57 +0100

    New release version 1.0.55.

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("UOInfusion")]
 [assembly: AssemblyProduct("Infusion")]
-[assembly: AssemblyCopyright("Copyright © UOInfusion 2017, 2018")]
+[assembly: AssemblyCopyright("Copyright © UOInfusion 2017-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.0.54.0")]
-[assembly: AssemblyFileVersion("1.0.54.0")]
+[assembly: AssemblyVersion("1.0.55.0")]
+[assembly: AssemblyFileVersion("1.0.55.0")]

c381e8a18a5f01b77b2bd99c75536e3675cd1adb
Jakub Linhart <jakub.linhart@gmail.com>
2019-03-25 15:20:09 +0100

    Version 1.0.64.

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.0.63.0")]
-[assembly: AssemblyFileVersion("1.0.63.0")]
+[assembly: AssemblyVersion("1.0.64.0")]
+[assembly: AssemblyFileVersion("1.0.64.0")]

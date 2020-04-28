399fdbd207230e2d4a4765a653876c73043f151b
3HMonkey <wewantthejoker@gmail.com>
2020-04-28 08:23:12 +0200

    Changes to packet TargetLocationRequest

--- Infusion/Packets/Client/TargetLocationRequest.cs
+++ Infusion/Packets/Client/TargetLocationRequest.cs
@@ -9,19 +9,20 @@ namespace Infusion.Packets.Client
             var payload = new byte[19];
             var writer = new ArrayPacketWriter(payload);
 
-            writer.WriteByte((byte) PacketDefinitions.TargetCursor.Id);
-            writer.WriteByte((byte) CursorTarget.Location);
-            writer.WriteUInt(cursorId.Value);
-            writer.WriteByte((byte) cursorType);
-            writer.WriteInt(0); // clicked on item = 0 (using location click)
+            writer.WriteByte((byte)PacketDefinitions.TargetCursor.Id);
+            writer.WriteByte((byte)CursorTarget.Location);
+            writer.WriteUInt(cursorId.Value); //TARGET SERIAL
+            writer.WriteByte((byte)cursorType); //FLAG
+            writer.WriteInt(0); // ALSO TARGET SERIAL FOR OBJECT PACKETS
             writer.WriteUShort((ushort)location.X);
             writer.WriteUShort((ushort)location.Y);
-            writer.WriteByte(0); // unknown
-            writer.WriteSByte((sbyte)location.Z);
-            writer.WriteModelId(tileType);
+            writer.WriteUShort((ushort)location.Z);
+            writer.WriteUShort((ushort)tileType); 
+
 
             RawPacket = new Packet(PacketDefinitions.TargetCursor.Id, payload);
         }
+
 
         public TargetLocationRequest(CursorId cursorId, ObjectId itemId, CursorType cursorType, Location3D location,
             ModelId itemType)

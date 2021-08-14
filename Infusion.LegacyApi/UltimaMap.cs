954638de9a0edc03b52a5d429324c0bdc04b95a5
Jakub Linhart <jakub.linhart@gmail.com>
2021-08-14 20:32:39 +0200

    Small map handling improvements.

--- Infusion.LegacyApi/UltimaMap.cs
+++ Infusion.LegacyApi/UltimaMap.cs
@@ -10,22 +10,27 @@ namespace Infusion.LegacyApi
         {
             var target = start.LocationInDirection(direction);
 
-            var tiles = Map.Felucca.Tiles.GetStaticTiles(target.X, target.Y);
-            if (tiles.Length != 0)
-            {
-                if (tiles.Any(t => TileData.ItemTable[t.ID].Impassable))
-                    return false;
-            }
-
             var targetLand = Map.Felucca.Tiles.GetLandTile(target.X, target.Y);
-            if (TileData.LandTable[targetLand.ID].Flags.HasFlag(TileFlag.Impassable))
-                return false;
-
             var startLand = Map.Felucca.Tiles.GetLandTile(start.X, start.Y);
             if (Math.Abs(targetLand.Z - startLand.Z) > 10)
                 return false;
 
-            return true;
+            var tiles = Map.Felucca.Tiles.GetStaticTiles(target.X, target.Y);
+            if (tiles.Length != 0)
+            {
+                var startSurfaceTiles = Map.Felucca.Tiles.GetStaticTiles(start.X, start.Y).Where(t => TileData.ItemTable[t.ID].Flags.HasFlag(TileFlag.Background));
+                var surfaceZ = startSurfaceTiles.Any() ? startSurfaceTiles.First().Z : startLand.Z;
+                if (tiles.Any(t => t.Z == surfaceZ && TileData.ItemTable[t.ID].Impassable))
+                    return false;
+            }
+            else
+            {
+                if (TileData.LandTable[targetLand.ID].Flags.HasFlag(TileFlag.Impassable))
+                    return false;
+            }
+
+            var groundItems = UO.Items.OnGround().Where(i => i.Location == target && TileData.ItemTable[i.Type].Impassable);
+            return !groundItems.Any();
         }
     }
 }
